7a720c773d9ada85e657f7faebee46a7167cb6ad
Ujjwal Jain <ujjwal.jain@vanderbilt.edu>
2022-11-17 07:04:48 -0600

    unit tests

--- app/utils.test.ts
+++ app/utils.test.ts
@@ -1,4 +1,4 @@
-import { validateEmail } from "./utils";
+import { safeRedirect, validateEmail, DEFAULT_REDIRECT } from "./utils";
 
 test("validateEmail returns false for non-emails", () => {
   expect(validateEmail(undefined)).toBe(false);
@@ -11,3 +11,14 @@ test("validateEmail returns false for non-emails", () => {
 test("validateEmail returns true for emails", () => {
   expect(validateEmail("kody@example.com")).toBe(true);
 });
+
+// write a test for the safeRedirect function
+test("safeRedirect returns default redirect for invalid input", () => {
+  expect(safeRedirect(undefined)).toBe(DEFAULT_REDIRECT);
+  expect(safeRedirect(null)).toBe(DEFAULT_REDIRECT);
+  expect(safeRedirect("")).toBe(DEFAULT_REDIRECT);
+  expect(safeRedirect("not-a-url")).toBe(DEFAULT_REDIRECT);
+  expect(safeRedirect("//not-a-url")).toBe(DEFAULT_REDIRECT);
+});
+
+// write a test to check if createCompetency returns a competency
