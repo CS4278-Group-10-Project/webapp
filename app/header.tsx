73db6c833d44da774192fb3c2aa1276b69b61723
saydus <saydus07@gmail.com>
2022-11-16 18:01:08 -0600

    Header reimplement

--- app/header.tsx
+++ app/header.tsx
@@ -6,65 +6,92 @@ import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 import { Form } from "@remix-run/react";
 import { useOptionalUser } from "./utils";
-import { Button, Link } from "@mui/material";
+import {
+  Button,
+  Link,
+  ListItem,
+  ListItemButton,
+  ListItemText,
+} from "@mui/material";
+
+const HeaderItem = ({ title, link }: { title: string; link: string }) => {
+  return (
+    <Button>
+      <Link href={link} sx={{ color: "#fff", textDecoration: "none" }}>
+        {title}
+      </Link>
+    </Button>
+  );
+};
+
+const LogOutButton = () => {
+  const onClick = () => {
+    // send post request to logout
+    fetch("/logout", {
+      method: "POST",
+    }).then(() => {
+      window.location.replace("/");
+    });
+  };
+
+  return (
+    <Button onClick={onClick} sx={{ color: "#fff" }}>
+      Logout
+    </Button>
+  );
+};
+
+const HEADER_ITEMS = [
+  { title: "Dashboard", link: "/" },
+  { title: "Courses", link: "/courses" },
+  { title: "Competencies", link: "/competencies" },
+  { title: "Activities", link: "/activities" },
+  { title: "Badges", link: "/badges" },
+];
+const LoggedInHeaderItems = () => {
+  return (
+    <>
+      <Box sx={{ display: { xs: "none", sm: "block" } }}>
+        {HEADER_ITEMS.map(({ title, link }) => (
+          <HeaderItem title={title} link={link} key={title} />
+        ))}
+
+        <LogOutButton />
+      </Box>
+    </>
+  );
+};
+
+const LoggedOutHeaderItems = () => {
+  return (
+    <>
+      <HeaderItem title="Register" link="/register" />
+      <HeaderItem title="Login" link="/login" />
+    </>
+  );
+};
 
 export default function Header() {
   const user = useOptionalUser();
   return (
     <Box sx={{ display: "flex" }}>
-      <AppBar
-        position="static"
-        style={{ backgroundColor: "#2E3B55" }}
-        component="nav"
-      >
+      <AppBar position="static" component="nav">
         <Toolbar>
-          <Link href="/" style={{ flexGrow: "1" }} className="xs-none sm-block">
-            <Typography variant="h6" component="div">
+          <Link href="/" style={{ flexGrow: "1" }}>
+            <Typography
+              variant="h6"
+              component="div"
+              sx={{
+                flexGrow: 1,
+                display: { xs: "none", sm: "block" },
+                color: "#fff",
+              }}
+            >
               VUSN
             </Typography>
           </Link>
-          {user ? (
-            <>
-              <Box
-                sx={{ display: { xs: "none", sm: "block" } }}
-                marginRight={2}
-              >
-                <Link href="/dashboard/enrollCourses">Course List</Link>
-              </Box>
-              <Box
-                sx={{ display: { xs: "none", sm: "block" } }}
-                marginRight={2}
-              >
-                <Link href="/dashboard">Dashboard</Link>
-              </Box>
-              <Box sx={{ display: { xs: "none", sm: "block" } }}>
-                <Form action="/logout" method="post">
-                  <Button
-                    type="submit"
-                    style={{ color: "white", textTransform: "none" }}
-                  >
-                    <Typography
-                      style={{ color: "#1976d2", textDecoration: "underline" }}
-                    >
-                      Logout
-                    </Typography>
-                  </Button>
-                </Form>
-              </Box>
-            </>
-          ) : (
-            <>
-              <Box
-                sx={{ display: { xs: "none", sm: "block" } }}
-                marginRight={2}
-              >
-                <Link href="/Register">Register</Link>
-              </Box>
-              <Box sx={{ display: { xs: "none", sm: "block" } }}>
-                <Link href="/login">Login</Link>
-              </Box>
-            </>
-          )}
+
+          {user ? <LoggedInHeaderItems /> : <LoggedOutHeaderItems />}
         </Toolbar>
       </AppBar>
     </Box>
