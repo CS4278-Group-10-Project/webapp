b158da4366cd06a767cfce7603906dffacbeb64a
saydus <saydus07@gmail.com>
2022-11-17 02:21:01 -0600

    made competency card and various actions

---
+++ app/models/competencies.server.ts
@@ -0,0 +1,27 @@
+import { Competency } from "@prisma/client";
+import { prisma } from "~/db.server";
+
+export async function getAllCompetencies(request: Request) {
+  return await prisma.competency.findMany();
+}
+
+export async function deleteCompetency({ id }: { id: Competency["id"] }) {
+  return await prisma.competency.delete({
+    where: { id },
+  });
+}
+
+export async function editCompetency({
+  id,
+  name,
+  description,
+}: {
+  id: Competency["id"];
+  name: Competency["name"];
+  description: Competency["description"];
+}) {
+  return await prisma.competency.update({
+    where: { id },
+    data: { name, description },
+  });
+}
--- app/routes/competencies.tsx
+++ app/routes/competencies.tsx
@@ -1,3 +1,218 @@
+import { getFullStudentUser, getUserId } from "~/session.server";
+import { UserType } from ".prisma/client";
+import { ActionArgs, json, LoaderArgs, redirect } from "@remix-run/node";
+import { Form, useLoaderData } from "@remix-run/react";
+
+import {
+  Box,
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogTitle,
+  Grid,
+  TextField,
+  Typography,
+} from "@mui/material";
+import { useState } from "react";
+import {
+  deleteCompetency,
+  editCompetency,
+  getAllCompetencies,
+} from "~/models/competencies.server";
+
+export async function loader({ request }: LoaderArgs) {
+  const user = await getFullStudentUser(request);
+  if (!user) {
+    return redirect("/login");
+  }
+  const competencies = await getAllCompetencies(request);
+
+  return json({
+    competencies: competencies,
+    isProfessor: user.accountType === UserType.PROFESSOR,
+  });
+}
+
+export async function action({ request }: ActionArgs) {
+  const userId = await getUserId(request);
+  if (!userId) {
+    return redirect("/login");
+  }
+  const formData = await request.formData();
+  const action = formData.get("action");
+  const id = formData.get("competencyId");
+
+  if (action === "delete") {
+    await deleteCompetency({ id: id as string });
+    return redirect("/competencies");
+  } else if (action === "edit") {
+    const name = formData.get("name");
+    const description = formData.get("description");
+
+    console.log(name, description);
+
+    await editCompetency({
+      id: id as string,
+      name: name as string,
+      description: description as string,
+    });
+
+    return redirect("/competencies");
+  }
+}
+
+function ViewCompetencyDialog({
+  competency,
+  open,
+  setModalOpen,
+}: {
+  competency: any;
+  open: boolean;
+  setModalOpen: (open: boolean) => void;
+}) {
+  return (
+    <Dialog
+      open={open}
+      onClose={() => setModalOpen(false)}
+      aria-labelledby="alert-dialog-title"
+      aria-describedby="alert-dialog-description"
+    >
+      <DialogTitle id="alert-dialog-title">{competency.name}</DialogTitle>
+      <DialogContent>
+        <Typography variant="body2" color="text.secondary">
+          {competency.description}
+        </Typography>
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={() => setModalOpen(false)} autoFocus>
+          Close
+        </Button>
+      </DialogActions>
+    </Dialog>
+  );
+}
+
+function EditCompetencyDialog({
+  competency,
+  open,
+  setModalOpen,
+}: {
+  competency: any;
+  open: boolean;
+  setModalOpen: (open: boolean) => void;
+}) {
+  return (
+    <Dialog
+      open={open}
+      onClose={() => setModalOpen(false)}
+      aria-labelledby="alert-dialog-title"
+      aria-describedby="alert-dialog-description"
+    >
+      <DialogTitle id="alert-dialog-title">Edit Competency</DialogTitle>
+      <Form method="post">
+        <DialogContent>
+          <input type="hidden" name="competencyId" value={competency.id} />
+          <TextField
+            autoFocus
+            margin="dense"
+            name="name"
+            label="Name"
+            type="text"
+            fullWidth
+            defaultValue={competency.name}
+          />
+          <TextField
+            margin="dense"
+            name="description"
+            label="Description"
+            type="text"
+            fullWidth
+            defaultValue={competency.description}
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={() => setModalOpen(false)} autoFocus>
+            Close
+          </Button>
+          <Button type="submit" name="action" value="edit" autoFocus>
+            Save
+          </Button>
+          <Button type="submit" name="action" value="delete">
+            Delete
+          </Button>
+        </DialogActions>
+      </Form>
+    </Dialog>
+  );
+}
+
+function CompetencyCard({
+  competency,
+  isProfessor,
+}: {
+  competency: any;
+  isProfessor: boolean;
+}) {
+  const [modalOpen, setModalOpen] = useState(false);
+  const [editModalOpen, setEditModalOpen] = useState(false);
+
+  return (
+    <Box p={2}>
+      <Card>
+        <CardContent>
+          <Typography
+            gutterBottom
+            variant="h5"
+            component="div"
+            noWrap
+            onClick={() => setModalOpen(true)}
+          >
+            {competency.name}
+          </Typography>
+          <Typography variant="body2" color="text.secondary" noWrap>
+            {competency.description}
+          </Typography>
+        </CardContent>
+        <CardActions>
+          <Button onClick={() => setModalOpen(true)}>View</Button>
+          {isProfessor && (
+            <Button onClick={() => setEditModalOpen(true)}>Edit</Button>
+          )}
+        </CardActions>
+      </Card>
+      <ViewCompetencyDialog
+        competency={competency}
+        open={modalOpen}
+        setModalOpen={setModalOpen}
+      />
+      <EditCompetencyDialog
+        competency={competency}
+        open={editModalOpen}
+        setModalOpen={setEditModalOpen}
+      />
+    </Box>
+  );
+}
+
 export default function Competencies() {
-  return <>Competencies</>;
+  const { competencies, isProfessor } = useLoaderData();
+
+  return (
+    <main
+      className="sm:items-top sm:justify-left relative h-full min-h-screen items-stretch bg-white sm:flex"
+      style={{ padding: "0 20px" }}
+    >
+      <Grid container spacing={1} mt={3}>
+        {competencies.map((competency: any, key: any) => (
+          <Grid item xs={3} key={key}>
+            <CompetencyCard competency={competency} isProfessor={isProfessor} />
+          </Grid>
+        ))}
+      </Grid>
+    </main>
+  );
 }
